7d5539ae03b1fb387dbb5a3babd762518bcfa63d
flip <dmobl30@wgu.edu>
2024-10-19 12:35:40 -0500

    Set up constants module and import it into main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import constant
+
 def main():
     print("Starting asteroids!")
+    print("Screen width: 1280")
+    print("Screen height: 720")
 
 if __name__ == "__main__":
     main()
