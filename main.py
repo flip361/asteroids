7cf94a0d5f5367b1aeb35f9609c0307332461dc1
flip <dmobl30@wgu.edu>
2024-10-20 12:56:12 -0500

    Ch2 Lesson6

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import time
 from constants import *
 from player import Player
 
@@ -27,6 +28,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.update(dt)
     
 
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
+from constants import PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -10,6 +11,19 @@ class Player(CircleShape):
     # Set the rotation attribute
     # in the player class
     
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # Rotate left: pass a negative delta time
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            # Rotate right: pass a positive delta time
+            self.rotate(dt)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
